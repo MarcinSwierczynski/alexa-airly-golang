9d2ac6829d60594f8cfbd460a8b9a7354606ed1c
Marcin Świerczyński <marcin@swierczynski.net>
2024-02-26 14:54:47 +0100

    Enhance the parser test

--- airly/response_parser_test.go
+++ airly/response_parser_test.go
@@ -14,11 +14,15 @@ func TestParseJsonResponse(t *testing.T) {
 
 	json := parseJsonResponse(file)
 
-	if len(json.Current.Indexes[0].Description) <= 0 {
-		t.Fatal("Description cannot be empty")
+	if json.Current.Indexes[0].Description != "Great air here today!" {
+		t.Fatal("Description needs to be proper")
 	}
 
-	if json.Current.Indexes[0].Value <= 0 {
-		t.Fatal("CAQI cannot be negative")
+	if json.Current.Indexes[0].Value != 7.43 {
+		t.Fatal("CAQI needs to be proper")
+	}
+
+	if json.Current.Standards[0].Percent != 29.71 {
+		t.Fatal("PM2.5 needs to be proper")
 	}
 }
